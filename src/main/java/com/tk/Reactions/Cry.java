5ae5fe054702a8fd351b9e671adb7bbcff018266
Guilherme Rialli Oliveira <96089204+guirialli@users.noreply.github.com>
2022-05-27 15:27:21 -0300

    bug fix in Cry

--- src/main/java/com/tk/Reactions/Cry.java
+++ src/main/java/com/tk/Reactions/Cry.java
@@ -5,7 +5,7 @@ import net.dv8tion.jda.api.entities.MessageChannel;
 public class Cry extends ReactingServices {
 
     public static void cry(MessageChannel channel, String dedicate, int number, String authorName ){
-        String path = "Animations/Cry";
+        String path = "Animations/Cry/";
         String afection="Cry, Cry with a baby";
         com.tk.Reactions.ReactingServices.sendMessage(
             com.tk.Reactions.ReactingServices.mountMessage("Cryng because", 
